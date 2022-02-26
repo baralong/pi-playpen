23d6f1050fe062f4d80a6291a9a88cf302fb7d92
baralong <doug.paice@baralong.org>
2022-02-26 19:07:16 +0800

    basic button press working

--- startup/DevPiStartUp.py
+++ startup/DevPiStartUp.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 # startup service for the devpi with the buttonshim and the LED shim
 from contextlib import nullcontext
+from datetime import datetime
+from itertools import takewhile
+import math
 import signal
+import threading
 import buttonshim
 from enum import Enum
 import ledshim
@@ -11,94 +15,71 @@ import subprocess
 allButtons = [buttonshim.BUTTON_A, buttonshim.BUTTON_B, buttonshim.BUTTON_C, buttonshim.BUTTON_D, buttonshim.BUTTON_E]
 class ButtonState(Enum):
     # really a "pressed" event should be triggered on release
-    # then we could have another state for waiting.
-    # the state machine is:
-    #   unpressed
-    #       => press
-    #           => release => trigger "tapped" => unpressed
-    #           => hold => trigger "held"
-    #               => release => unpressed
     UNPRESSED = 0
-    PRESSED = 1
+    PRESSED = 1 # triggered on release IIF on_hold wasn't triggered 
     HELD = 2
-    RELEASED = 3
 
-class Button:
-    state = ButtonState.UNPRESSED
-    def press(self):
-        self.state = ButtonState.PRESSED
-    def hold(self):
-        self.state = ButtonState.HELD
-    def release(self):
-        self.state = ButtonState.RELEASED
-    def reset(self):
-        self.state = ButtonState.RELEASED
+buttonStates = [ButtonState.UNPRESSED] * 5
+ledLock = threading.Lock()
 
-tmp = Button()
-states = [Button(), Button(), Button(), Button(), Button() ]
+ledshim.clear()
+ledshim.show()
+
 @buttonshim.on_press(allButtons)
-def handle_button(button, state): # the state parameter is so you can reuse the same handler for press and release
-    global states
-    global tmp
-    states[button].press()
-    print("PRESS: Button {} ({}) is {}".format(button, buttonshim.NAMES[button], state))
+def handle_press(button, state):
+    global ledLock
+    exit_time = time.perf_counter() + 0.3 
+    with ledLock:
+        step = 0
+        button_pixel = ledshim.width - 3 - round(button * 3.75)
+        pixels = range(button_pixel - 3, button_pixel)
+        while time.perf_counter() < exit_time:
+            ledshim.set_multiple_pixels(pixels, (255,0,0))
+            ledshim.show()
+            time.sleep(0.05)
+            ledshim.clear()
+            ledshim.show()
+            time.sleep(0.05)
 
-@buttonshim.on_hold(allButtons)
+@buttonshim.on_hold(allButtons, hold_time = 1)
 def handle_hold(button):
-    global states
-    states[button].hold()
-    # this is where we call the state handler
-    print("HOLD: Button {} ({})".format(button, buttonshim.NAMES[button]))
+    global buttonStates
+    buttonStates[button] = ButtonState.HELD
 
 @buttonshim.on_release(allButtons)
 def handle_release(button, state):
-    global states
-    if states[button].state == ButtonState.PRESSED:
-        # this is where we call the state handler, just before the press finishes....hmm
-        a = 1
-    states[button].release()
-    print("RELEASE: Button {} ({}) is {}".format(button, buttonshim.NAMES[button], state))
+    global buttonStates
+    if buttonStates[button] == ButtonState.HELD:
+        buttonStates[button] = ButtonState.UNPRESSED
+    else:
+        buttonStates[button] = ButtonState.PRESSED
 
 
-# not happy with this I think newing up an iterator might be the go, but I 
-# want to be able to break out... it's important to know when to confinue
-# or  start again
-class handler:
-    # does the current state match this handler
-    def match(self, state):
-        return False
-    # do the thing, return True if you have more to do 
-    def do(self):
-        pass
-    def done(self):
-        return True
+# class handler:
+#     # does the current state match this handler
+#     def match(self, state):
+#         return False
+#     def launch(self):
+#         pass
 
-class start_handler(handler):
-    count = 0
-    def match(self, state):
-        return self.count == 0
-    def do(self):
-        self.count += 1
-        return self.count < 100
+# class start_handler(handler):
+#     count = 0
+#     def match(self, state):
+#         return self.count == 0
+#     def __iter__(self):
+#         return self
+#     def __next__(self):
+#         raise StopIteration
 
-class rainbow_handler(handler):
-    pass
+# class rainbow_handler(handler):
+#     pass
 
 
 
-handlers = [
-    start_handler(),
-    rainbow_handler()
-]
+# handlers = [
+#     start_handler(),
+#     rainbow_handler(),
+#     handler()
+# ]
 
-
-
-currentHandler = handler()
-while True:
-    for handler in handlers:
-        if handler.match(states):
-            currentHandler = handler
-            break
-    if not currentHandler.done():
-        currentHandler.do()
-    time.sleep(0.01)
+signal.pause()
