1ebc7badd6dd620dc38f3452c9b8d4ca33abd340
Doug Paice <doug.paice@riotinto.com>
2022-03-21 13:06:30 +0800

    use nre font

--- startup/GadgetPiStartup.py
+++ startup/GadgetPiStartup.py
@@ -3,14 +3,13 @@ from inky.auto import auto
 from PIL import Image, ImageFont, ImageDraw
 import ifcfg
 import socket
+import os
 
 inky_display = auto()
 inky_display.set_border(inky_display.WHITE)
 
 img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
 draw = ImageDraw.Draw(img)
-from font_fredoka_one import FredokaOne
-from font_source_sans_pro import SourceSansPro
 
 hostname = socket.gethostname()
 net = ifcfg.interfaces() # this is a dictionary,. I want wlan0 and usb0, but want to check if present first
@@ -20,8 +19,10 @@ if('wlan0' in net and 'inet' in net['wlan0'] and net['wlan0']['inet'] is not Non
 if('usb0' in net and 'inet' in net['usb0'] and net['usb0']['inet'] is not None): 
     text += 'u:' + net['usb0']['inet']
 
-font = ImageFont.truetype(SourceSansPro, 15)
-draw.text((10, 10), text, inky_display.BLACK, font)
+drawFont = ImageFont.truetype(
+    font=os.path.dirname(os.path.realpath(__file__)) + '/Perfect DOS VGA 437.ttf', 
+    size=16) # multiples of 8
+draw.text((10, 10), text, inky_display.BLACK, drawFont)
 
 inky_display.set_image(img)
 inky_display.show()
