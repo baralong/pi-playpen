bccb723dbd38372de2dc2b082cc7bc5970f22933
Doug Paice <doug.paice@baralong.org>
2022-03-28 12:41:32 +0800

    chart on top

--- startup/GadgetPiStartup.py
+++ startup/GadgetPiStartup.py
@@ -28,14 +28,13 @@ y = 10 # start 10 pixles down
 hostname = socket.gethostname()
 font = ImageFont.truetype(vga_font,24)
 w, h = font.getsize(hostname)
-x = (inky_display.WIDTH / 2) - (w / 2)
+
 
 # draw a box around the text with 5 pixel padding
-draw.rounded_rectangle([(x-7,y-5),(x+w+5, y+h+5)],
+draw.rounded_rectangle([(5,y-5),(w+10, y+h+5)],
                        5, inky_display.RED, inky_display.BLACK, 2)
-draw.text((x, y), hostname, inky_display.WHITE, font)
+draw.text((10, y), hostname, inky_display.WHITE, font)
 y += h + 15
-y2=y
 
 font_awesome_solid = ImageFont.truetype('./Font Awesome 6 Free-Solid-900.otf', 16)
 font_awesome_brands = ImageFont.truetype('./Font Awesome 6 Brands-Regular-400.otf', 16)
@@ -58,44 +57,57 @@ if('usb0' in net and 'inet' in net['usb0'] and net['usb0']['inet'] is not None):
 if(getpass.getuser() == 'root'):
     os.system('umount /usbdisk.d')
     os.system('mount -o loop -t exfat /usbdisk.img /usbdisk.d')
-    print('remounted')
+    #print('remounted')
 
 usage = shutil.disk_usage("/usbdisk.d")
 font = ImageFont.truetype(vga_font, 16)
 
-text = f'u:{format_bytes(usage.used)}'
-w, h = font.getsize(text)
-x = 3
-y = inky_display.HEIGHT - h - 4
-draw.rectangle(
-        xy = [(0, y-2),(inky_display.WIDTH-1, inky_display.HEIGHT-1)],
+labels = 'Used\nFree\nTotal'
+lw,lh = draw.multiline_textsize(
+            text = labels, 
+            font = font, 
+            spacing = 2)
+
+values = (f'{format_bytes(usage.used)}\n'+
+          f'{format_bytes(usage.free)}\n'+
+          f'{format_bytes(usage.total)}')
+
+vw,vh = draw.multiline_textsize(
+            text = values, 
+            font = font, 
+            spacing = 2)
+ 
+bx = inky_display.WIDTH - 3 - (lw + vw+ 5) - 10 # border, text width, box padding 
+by = inky_display.HEIGHT - 3 - lh - 10 #border, text height, box padding
+draw.rounded_rectangle(
+        xy = [(bx,by),(inky_display.WIDTH-1, inky_display.HEIGHT-1)],
         fill = inky_display.RED,
         outline = inky_display.BLACK,
-        width = 1)
+        width = 1,
+        radius = 5)
+tx = bx + 6
+ty = by + 4
+draw.multiline_text(xy = (tx,ty), 
+                    text = labels, 
+                    fill = inky_display.WHITE, 
+                    font = font,
+                    align = 'left')
+draw.multiline_text(xy = (inky_display.WIDTH - 4 - vw,ty), 
+                    text = values, 
+                    fill = inky_display.WHITE, 
+                    font = font,
+                    align = 'right')
 
-draw.text((x,y), text, inky_display.WHITE, font)
-
-text = f'f:{format_bytes(usage.free)}'
-w, h = font.getsize(text)
-x = (inky_display.WIDTH - w)/2
-draw.text((x,y), text, inky_display.WHITE, font)
-
-text = f't:{format_bytes(usage.total)}'
-w, h = font.getsize(text)
-x = inky_display.WIDTH - 4 - w
-draw.text((x,y), text, inky_display.WHITE, font)
-
-y -= 6 # space above
-
-usageSize = y - y2
-draw.ellipse(xy = [(inky_display.WIDTH - usageSize, y2),
-                   (inky_display.WIDTH-2, y-2)],
+# find the biggest box for the pie chart
+bw = inky_display.WIDTH - bx
+usageSize = min(bw - 5,by - 6) 
+chartx = bx + (0.5*(bw - usageSize)) + 2
+draw.ellipse(xy =  [(chartx, 2), (chartx+usageSize, usageSize + 2)],
              outline = inky_display.BLACK, 
              fill = inky_display.WHITE, 
              width = 2)
 
-draw.pieslice(xy = [(inky_display.WIDTH - usageSize, y2),
-                 (inky_display.WIDTH-2, y-2)],
+draw.pieslice(xy = [(chartx, 2), (chartx+usageSize, usageSize + 2)],
              fill = inky_display.RED, 
              width = 2,
              start = 0,
