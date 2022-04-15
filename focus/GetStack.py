502185aa10e4d5a1ffefc89708891683de3c43a9
Doug Paice <doug.paice@baralong.org>
2022-04-15 10:19:21 +0800

    Stack working

--- focus/GetStack.py
+++ focus/GetStack.py
@@ -11,25 +11,21 @@ height = 3496
 
 # Instantiate the libcamera class
 cam = libcamera.libcamera()
-input("pausing")
 ret = cam.initCamera(width, height, libcamera.PixelFormat.RGB888, buffercount=1, rotation=0)
-input(f'init {ret}')
 ret = cam.startCamera()
-input(f'start {ret}')
 if ret != 0: 
     cam.stopCamera()
     cam.closeCamera()
     exit()
-
-input("post allocation")
+input("init")
 focuser.set(Focuser.OPT_FOCUS, 4095)
 
 startTime = datetime.now().strftime('%Y%m%dt%H%M')
 path = f'/home/pi/stack/D{startTime}'
 Path(path).mkdir(parents=True, exist_ok=True)
-
+time.sleep(0.5)
 start = time.perf_counter()
-for focus in range(4095, 2000, -64):
+for focus in range(4095, 2000, -50):
     focuser.set(Focuser.OPT_FOCUS, focus)
     ret, data = cam.readFrame()
     if not ret:
